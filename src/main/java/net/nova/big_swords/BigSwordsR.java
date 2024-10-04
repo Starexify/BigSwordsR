12e9858ef4e7bebc72fbce15a42a478a641adade
Starexify <nightbear843@yahoo.com>
2024-10-04 23:43:24 +0200

    1.20.1 Porting

---
+++ src/main/java/net/nova/big_swords/BigSwordsR.java
@@ -0,0 +1,19 @@
+package net.nova.big_swords;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static net.nova.big_swords.BigSwordsR.MODID;
+
+@Mod(MODID)
+public class BigSwordsR {
+    public static final String MODID = "big_swords";
+    public static final Logger LOGGER = LoggerFactory.getLogger(BigSwordsR.class);
+
+    public BigSwordsR(FMLJavaModLoadingContext context) {
+        IEventBus bus = context.getModEventBus();
+    }
+}
