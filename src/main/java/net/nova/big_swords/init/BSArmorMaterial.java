12497678a9a0baa0a618389f49d4f63182e55d15
Starexify <nightbear843@yahoo.com>
2024-11-17 15:21:29 +0100

    1.0.2

--- src/main/java/net/nova/big_swords/init/BSArmorMaterial.java
+++ src/main/java/net/nova/big_swords/init/BSArmorMaterial.java
@@ -1,45 +1,27 @@
 package net.nova.big_swords.init;
 
-import com.google.common.base.Suppliers;
 import net.minecraft.Util;
-import net.minecraft.core.Holder;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorType;
-import net.neoforged.neoforge.registries.DeferredRegister;
-import net.nova.big_swords.BigSwordsR;
 import net.nova.big_swords.equipment.BSEquipmentModels;
 
 import java.util.EnumMap;
-import java.util.List;
-import java.util.function.Supplier;
-
-import static net.nova.big_swords.BigSwordsR.MODID;
 
 public class BSArmorMaterial {
-    public static Supplier<ArmorMaterial> LIVINGMETAL = Suppliers.memoize(() -> new ArmorMaterial(
-            29,
-            Util.make(new EnumMap<>(ArmorType.class), map -> {
-                map.put(ArmorType.BOOTS, 3);
-                map.put(ArmorType.LEGGINGS, 5);
-                map.put(ArmorType.CHESTPLATE, 7);
-                map.put(ArmorType.HELMET, 3);
-                map.put(ArmorType.BODY, 5);
-            }), 12, SoundEvents.ARMOR_EQUIP_IRON, 0.5F, 0.0F, Tags.BSItemTags.REPAIRS_LIVINGMETAL_ARMOR, BSEquipmentModels.LIVINGMETAL
-    ));
+    public static ArmorMaterial LIVINGMETAL = new ArmorMaterial(29, Util.make(new EnumMap<>(ArmorType.class), map -> {
+        map.put(ArmorType.BOOTS, 3);
+        map.put(ArmorType.LEGGINGS, 5);
+        map.put(ArmorType.CHESTPLATE, 7);
+        map.put(ArmorType.HELMET, 3);
+        map.put(ArmorType.BODY, 5);
+    }), 12, SoundEvents.ARMOR_EQUIP_IRON, 0.5F, 0.0F, Tags.BSItemTags.REPAIRS_LIVINGMETAL_ARMOR, BSEquipmentModels.LIVINGMETAL);
 
-    public static Supplier<ArmorMaterial> BIOMASS = Suppliers.memoize(() -> new ArmorMaterial(
-            29,
-            Util.make(new EnumMap<>(ArmorType.class), map -> {
-                map.put(ArmorType.BOOTS, 2);
-                map.put(ArmorType.LEGGINGS, 5);
-                map.put(ArmorType.CHESTPLATE, 7);
-                map.put(ArmorType.HELMET, 3);
-                map.put(ArmorType.BODY, 5);
-            }), 14, SoundEvents.ARMOR_EQUIP_GENERIC, 0.0F, 0.0F, Tags.BSItemTags.REPAIRS_BIOMASS_ARMOR, BSEquipmentModels.BIOMASS
-    ));
+    public static ArmorMaterial BIOMASS = new ArmorMaterial(29, Util.make(new EnumMap<>(ArmorType.class), map -> {
+        map.put(ArmorType.BOOTS, 2);
+        map.put(ArmorType.LEGGINGS, 5);
+        map.put(ArmorType.CHESTPLATE, 7);
+        map.put(ArmorType.HELMET, 3);
+        map.put(ArmorType.BODY, 5);
+    }), 14, SoundEvents.ARMOR_EQUIP_GENERIC, 0.0F, 0.0F, Tags.BSItemTags.REPAIRS_BIOMASS_ARMOR, BSEquipmentModels.BIOMASS);
 }
