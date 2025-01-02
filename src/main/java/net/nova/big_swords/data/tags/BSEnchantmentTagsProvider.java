51ab879107de8610286c33724737a218217c5517
Starexify <nightbear843@yahoo.com>
2025-01-02 23:29:30 +0100

    Ench Tag

--- src/main/java/net/nova/big_swords/data/tags/BSEnchantmentTagsProvider.java
+++ src/main/java/net/nova/big_swords/data/tags/BSEnchantmentTagsProvider.java
@@ -3,6 +3,7 @@ package net.nova.big_swords.data.tags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EnchantmentTagsProvider;
+import net.minecraft.tags.EnchantmentTags;
 import net.nova.big_swords.data.BSEnchantments;
 import net.nova.big_swords.init.Tags;
 
@@ -17,7 +18,7 @@ public class BSEnchantmentTagsProvider extends EnchantmentTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
-        tag(Tags.EnchantmentTags.IN_ENCHANTING_TABLE).addOptional(BSEnchantments.SOUL_STEALER.location());
+        tag(EnchantmentTags.IN_ENCHANTING_TABLE).addOptional(BSEnchantments.SOUL_STEALER.location());
         tag(Tags.EnchantmentTags.SCYTHE_EXCLUSIVE).addOptional(BSEnchantments.SOUL_STEALER.location());
     }
 }
