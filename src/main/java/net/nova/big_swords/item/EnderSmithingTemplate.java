43612f338a3caf1653b0e6ed071ab7af758268c1
Starexify <nightbear843@yahoo.com>
2025-02-12 14:24:35 +0100

    Fixed Glaive not being damages on hit and ported all the 1.1.1 NeoForge version changelog

--- src/main/java/net/nova/big_swords/item/EnderSmithingTemplate.java
+++ src/main/java/net/nova/big_swords/item/EnderSmithingTemplate.java
@@ -6,7 +6,10 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
+import net.nova.big_swords.BigSwordsR;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static net.nova.big_swords.BigSwordsR.rl;
@@ -29,17 +32,19 @@ public class EnderSmithingTemplate extends SmithingTemplateItem {
             Util.createTranslationKey("item", rl("smithing_template.ender_upgrade.additions_slot_description"))
     );
 
-    public static final Identifier EMPTY_SLOT_HELMET = Identifier.ofVanilla("item/empty_armor_slot_helmet");
-    public static final Identifier EMPTY_SLOT_CHESTPLATE = Identifier.ofVanilla("item/empty_armor_slot_chestplate");
-    public static final Identifier EMPTY_SLOT_LEGGINGS = Identifier.ofVanilla("item/empty_armor_slot_leggings");
-    public static final Identifier EMPTY_SLOT_BOOTS = Identifier.ofVanilla("item/empty_armor_slot_boots");
-    public static final Identifier EMPTY_SLOT_HOE = Identifier.ofVanilla("item/empty_slot_hoe");
-    public static final Identifier EMPTY_SLOT_AXE = Identifier.ofVanilla("item/empty_slot_axe");
-    public static final Identifier EMPTY_SLOT_SWORD = Identifier.ofVanilla("item/empty_slot_sword");
-    public static final Identifier EMPTY_SLOT_SHOVEL = Identifier.ofVanilla("item/empty_slot_shovel");
-    public static final Identifier EMPTY_SLOT_PICKAXE = Identifier.ofVanilla("item/empty_slot_pickaxe");
-    public static final Identifier EMPTY_SLOT_BIG_SWORD = rl("item/empty_slot_big_sword");
-    public static final Identifier EMPTY_SLOT_INGOT = Identifier.ofVanilla("item/empty_slot_ingot");
+    public static final List<Identifier> INGREDIENT_SLOTS = new ArrayList<>(Arrays.asList(
+            Identifier.of("container/slot/helmet"),
+            Identifier.of("container/slot/chestplate"),
+            Identifier.of("container/slot/leggings"),
+            Identifier.of("container/slot/boots"),
+            Identifier.of("container/slot/hoe"),
+            Identifier.of("container/slot/axe"),
+            Identifier.of("container/slot/sword"),
+            Identifier.of("container/slot/shovel"),
+            Identifier.of("container/slot/pickaxe"),
+            BigSwordsR.rl("container/slot/big_sword")
+    ));
+    public static final Identifier EYE_SLOT = Identifier.ofVanilla("item/empty_slot_ingot");
 
     public EnderSmithingTemplate(Text appliesTo, Text ingredients, Text baseSlotDescription, Text additionsSlotDescription, List<Identifier> emptyBaseSlotTextures, List<Identifier> emptyAdditionsSlotTextures, Settings settings) {
         super(appliesTo, ingredients, baseSlotDescription, additionsSlotDescription, emptyBaseSlotTextures, emptyAdditionsSlotTextures, settings);
@@ -51,28 +56,13 @@ public class EnderSmithingTemplate extends SmithingTemplateItem {
                 ENDER_UPGRADE_INGREDIENTS,
                 ENDER_UPGRADE_BASE_SLOT_DESCRIPTION,
                 ENDER_UPGRADE_ADDITIONS_SLOT_DESCRIPTION,
-                getEnderUpgradeEmptyBaseSlotTextures(),
+                INGREDIENT_SLOTS,
                 getEnderUpgradeEmptyAdditionsSlotTextures(),
                 settings
         );
     }
 
-    public static List<Identifier> getEnderUpgradeEmptyBaseSlotTextures() {
-        return List.of(
-                EMPTY_SLOT_HELMET,
-                EMPTY_SLOT_SWORD,
-                EMPTY_SLOT_BIG_SWORD,
-                EMPTY_SLOT_CHESTPLATE,
-                EMPTY_SLOT_PICKAXE,
-                EMPTY_SLOT_LEGGINGS,
-                EMPTY_SLOT_AXE,
-                EMPTY_SLOT_BOOTS,
-                EMPTY_SLOT_HOE,
-                EMPTY_SLOT_SHOVEL
-        );
-    }
-
     public static List<Identifier> getEnderUpgradeEmptyAdditionsSlotTextures() {
-        return List.of(EMPTY_SLOT_INGOT);
+        return List.of(EYE_SLOT);
     }
 }
