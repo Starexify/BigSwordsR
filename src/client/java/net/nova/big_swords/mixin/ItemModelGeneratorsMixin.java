b3e6df08f0031ff8e707c87c64075ad68e499e73
ImVeryBad <nightbear843@yahoo.com>
2025-04-12 12:28:05 +0200

    Better approach without Injecting (thanks  LlamaLad7)

--- src/client/java/net/nova/big_swords/mixin/ItemModelGeneratorsMixin.java
+++ src/client/java/net/nova/big_swords/mixin/ItemModelGeneratorsMixin.java
@@ -6,30 +6,20 @@ import net.nova.big_swords.equipment.BSMaterialAssetGroup;
 import net.nova.big_swords.equipment.BSTrimMaterials;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
 @Mixin(ItemModelGenerators.class)
 public class ItemModelGeneratorsMixin {
-    @Mutable
     @Shadow
     @Final
     public static List<ItemModelGenerators.TrimMaterialData> TRIM_MATERIAL_MODELS;
 
-    @Inject(method = "<clinit>", at = @At("RETURN"))
-    private static void injectAdditionalTrimMaterial(CallbackInfo ci) {
-        try {
-            TRIM_MATERIAL_MODELS = ImmutableList.<ItemModelGenerators.TrimMaterialData>builder()
-                    .addAll(TRIM_MATERIAL_MODELS)
-                    .add(new ItemModelGenerators.TrimMaterialData(BSMaterialAssetGroup.LIVINGMETAL, BSTrimMaterials.LIVINGMETAL))
-                    .build();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    static {
+        TRIM_MATERIAL_MODELS = ImmutableList.<ItemModelGenerators.TrimMaterialData>builder()
+                .addAll(TRIM_MATERIAL_MODELS)
+                .add(new ItemModelGenerators.TrimMaterialData(BSMaterialAssetGroup.LIVINGMETAL, BSTrimMaterials.LIVINGMETAL))
+                .build();
     }
 }
